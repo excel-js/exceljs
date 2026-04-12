3e9cda123828596ad37d3b2e11420157207e33d7
Roman <dev@rman.dev>
2026-04-12 19:06:15 +0100

    remove browser builds

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,25 +1,9 @@
 import { defineConfig } from "tsdown";
-import nodePolyfills from "@rolldown/plugin-node-polyfills";
 
-export default defineConfig([
-  {
-    entry: { exceljs: "./lib/exceljs.browser.js" },
-    format: ["cjs"],
-    platform: "browser",
-    plugins: [nodePolyfills()],
-    exports: true,
+export default defineConfig({
+  entry: {
+    index: "./lib/exceljs.nodejs.js",
+    "exceljs.bare": "./lib/exceljs.bare.js",
   },
-
-  {
-    entry: { "exceljs.bare": "./lib/exceljs.bare.js" },
-    format: ["cjs"],
-    exports: true,
-  },
-
-  {
-    entry: { index: "./lib/exceljs.nodejs.js" },
-    format: ["cjs"],
-    platform: "node",
-    exports: true,
-  },
-]);
+  exports: true,
+});
