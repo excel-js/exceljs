92eb9e09c05aaaf2a0c1d586b2a504e51b593a73
Roman <dev@rman.dev>
2026-04-12 21:41:33 +0100

    wip

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: {
-    index: "./lib/exceljs.nodejs.js",
-    "exceljs.bare": "./lib/exceljs.bare.js",
-  },
+  entry: "./lib/exceljs.nodejs.js",
   exports: true,
 });
