6e38d4bd577ca85a6bcb29d5ab90e2dcf1bec44f
Roman <dev@rman.dev>
2026-04-12 14:38:22 +0100

    wip

--- spec/utils/verquire.js
+++ spec/utils/verquire.js
@@ -6,16 +6,6 @@
 const libs = {};
 const basePath = (function() {
   const nodeMajorVersion = parseInt(process.versions.node.split('.')[0], 10);
-  if (process.env.EXCEL_BUILD === 'es5' || nodeMajorVersion < 10) {
-    require('core-js/modules/es.promise');
-    require('core-js/modules/es.object.assign');
-    require('core-js/modules/es.object.keys');
-    require('core-js/modules/es.symbol');
-    require('core-js/modules/es.symbol.async-iterator');
-    require('regenerator-runtime/runtime');
-    libs.exceljs = require('../../dist/es5');
-    return '../../dist/es5/';
-  }
   libs.exceljs = require('../../lib/exceljs.nodejs');
   return '../../lib/';
 })();
